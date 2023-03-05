dc46d017f7a8fd506ddc769139eb18ecfe825365
Adedunmola <oyewaleadedunmola@gmail.com>
2023-03-05 15:08:10 +0100

    add nextIntervalDate function in utils and install dayjs

---
+++ src/utils/intervalDate.ts
@@ -0,0 +1,10 @@
+import { Card, Prisma } from '@prisma/client';
+import dayjs from 'dayjs';
+
+export const nextIntervalDate = (card: Card) => {
+    const now = dayjs()
+
+    const nextDateInDays = now.add(card.nextInterval, 'days')
+
+    return nextDateInDays.format()
+}
